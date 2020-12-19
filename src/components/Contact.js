edcb60fc740351e073f69185e8f22bd9ecc0a45b
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-12-18 20:55:46 -0500

    Implement sending email with emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,40 @@
-import{ init, emailjs } from 'emailjs-com';
+import emailjs from 'emailjs-com';
 import React, { useState } from 'react'
-init("user_DP6MP9QTuR99lXDoMUXCK");
 
 function Contact() {
-    const [userInput, setUserInput] = useState()
-    const handleChange = event => {
-        const {name, value} = event.target
-        setUserInput({
-            ...userInput,
-            [name]: value
-        })
-    }
-
+    const [responseMsg, setResponseMsg] = useState('')
+    
     const handleSubmit = event => {
         event.preventDefault()
-        // console.log(userInput)
-        // this.contact_number.value = Math.random() * 100000 | 0;
-                // these IDs from the previous steps
-                emailjs.sendForm('outlook', 'contact_form', event.target)
-                    .then(function() {
-                        console.log('SUCCESS!');
-                    }, function(error) {
-                        console.log('FAILED...', error);
-                    });
+        emailjs.sendForm('service_8d5zpkp', 'contact_form', 'msg-form', 'user_DP6MP9QTuR99lXDoMUXCK')
+        .then( response => {
+          setResponseMsg('Message sent. Thanks for Reaching out!')
+        }, error => {
+          setResponseMsg(`Message not sent. ${error}`)
+        });
     }
     return (
         <div className="contact-page">
             <p>Contact Me section</p>
-            <form id="contact_form" onSubmit={handleSubmit}>
+            <form id="msg-form" onSubmit={handleSubmit}>
                 <div className="name-section inputs">
                     <label htmlFor="name">Name</label>
-                    <input onChange={handleChange} type="text" placeholder="Full Name"  id="name" name="name"/>
+                    <input type="text" placeholder="Full Name"  id="name" name="name"/>
                 </div>
 
                 <div className="email-section inputs">
                     <label htmlFor="email">Email</label>
-                    <input onChange={handleChange} type="email" placeholder="Email"  id="email" name="email"/>
+                    <input type="email" placeholder="Email"  id="email" name="email"/>
                 </div>
 
                 <div className="msg-section inputs">
                     <label htmlFor="message">Message</label>
-                    <textarea onChange={handleChange} name="message" id="message" cols="30" rows="10" placeholder="Type your message"></textarea>
+                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Type your message"></textarea>
                 </div>
-                <button onClick={handleSubmit}>Send</button>
+                <button>Send</button>
+                <div className="box-info">
+                  <span>{ responseMsg }</span>
+                </div>
             </form>
         </div>
     )
