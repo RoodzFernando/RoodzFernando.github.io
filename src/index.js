156a3f8c789b1bf45fe8b58e96bcd5141b167513
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2021-01-11 22:28:35 -0500

    Import all the sass files in index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import AppRoutes from './routers/AppRoutes';
 import { Provider } from 'react-redux'
 import { store } from './redux/redux';
 import './styles/styles.scss';
+import './styles/components/Home.scss';
 import './styles/components/AsidePage.scss';
 import './styles/components/AsideNav.scss';
 import './styles/components/Project.scss';
 import './styles/components/About.scss';
 import './styles/components/Contact.scss';
+import './styles/components/DashBoard.scss';
+import './styles/components/ProjectCreate.scss';
+import './styles/components/ProjectUpdate.scss';
 import './styles/components/AppRoutes.scss';
+import './styles/components/Login.scss';
 import './styles/components/devices.scss';
 
 ReactDOM.render(
