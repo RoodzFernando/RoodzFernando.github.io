057b3a4cb46f7707d9c444361cb32539f5aa54f0
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2021-01-22 17:16:54 -0500

    Update the about me text

--- src/components/About.js
+++ src/components/About.js
@@ -8,13 +8,7 @@ function About() {
         <div className="about-page">
           <div className="about-text">
             <h1>About me</h1>
-            <p>I am a civil engineer who fell in love with
-             new technologies since I used and relied on 
-             it as a tool to bring sustainable solutions.
-              My admiration for technology has led me to 
-              learn web development where I have learned 
-              some programming languages, which helps me 
-              think abstractly, to bring concrete solutions.</p>
+            <p>I am a civil engineer who fell in love with new information technology. Since I have been using and relying on the web to solve problems, I develop a deep appreciation for web development. So I decided to learn how to develop websites. During my web development journey, I learned a few programming languages and a few other technological apparatus like data structures and algorithms to improve my thinking. I can say without a doubt that I am more equipped right now to come up with more effective solutions to almost every problem I face in my life..</p>
             <div className="signature">Roodz Fernando</div>
           </div>
         </div>
